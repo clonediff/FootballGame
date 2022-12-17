468aef879cd18e042c57f905b5ab39f8941ab90a
NailZinn <nailzinnurov17@gmail.com>
2022-12-17 22:30:28 +0300

    [update] game process

--- UIApplication/Views/GamePage.xaml.cs
+++ UIApplication/Views/GamePage.xaml.cs
@@ -1,4 +1,5 @@
 using UIApplication.Models;
+using Point = Microsoft.Maui.Graphics.Point;
 
 namespace UIApplication.Views;
 
@@ -7,6 +8,11 @@ public partial class GamePage : ContentPage
     private readonly IDispatcherTimer _timer;
     private TimeSpan _time = TimeSpan.FromMinutes(3);
 
+    private const int _collisionEps = 3;
+    private const int _translationWithBall = 4;
+    private const int _translationWithOutBall = 6;
+    private const double _angle = Math.PI / 10;
+    
     public GamePage(Game game)
     {
         InitializeComponent();
@@ -33,19 +39,257 @@ public partial class GamePage : ContentPage
         BindingContext = game;
     }
 
-    private void OnUpClickedTeamA(object sender, EventArgs e) => imgTeamA.TranslationY -= 10;
+    protected override void OnDisappearing()
+    {
+        if (_timer.IsRunning)
+            _timer.Stop();
+    }
 
-    private void OnDownClickedTeamA(object sender, EventArgs e) => imgTeamA.TranslationY += 10;
+    private void OnUpClickedTeam(object sender, EventArgs e)
+    {
+        if (((ImageButton)sender).StyleId.EndsWith("A"))
+        {
+            Go(imgTeamA, imgTeamB, imgBall, 
+                (deltaY) => imgTeamA.TranslationY -= deltaY, 
+                (deltaY) => imgBall.TranslationY -= deltaY,
+                (player, item) => player.Y > item.Y);
+        }
+        else
+        {
+            Go(imgTeamB, imgTeamA, imgBall, 
+                (deltaY) => imgTeamB.TranslationY -= deltaY, 
+                (deltaY) => imgBall.TranslationY -= deltaY,
+                (player, item) => player.Y > item.Y);
+        }
+    }
 
-    private void OnLeftClickedTeamA(object sender, EventArgs e) => imgTeamA.TranslationX -= 10;
+    private void OnDownClickedTeam(object sender, EventArgs e)
+    {
+        if (((ImageButton)sender).StyleId.EndsWith("A"))
+        {
+            Go(imgTeamA, imgTeamB, imgBall, 
+                (deltaY) => imgTeamA.TranslationY += deltaY, 
+                (deltaY) => imgBall.TranslationY += deltaY,
+                (player, item) => player.Y < item.Y);
+        }
+        else
+        {
+            Go(imgTeamB, imgTeamA, imgBall, 
+                (deltaY) => imgTeamB.TranslationY += deltaY, 
+                (deltaY) => imgBall.TranslationY += deltaY,
+                (player, item) => player.Y < item.Y);
+        }
+    }
 
-    private void OnRightClickedTeamA(object sender, EventArgs e) => imgTeamA.TranslationX += 10;
+    private void OnLeftClickedTeam(object sender, EventArgs e)
+    {
+        if (((ImageButton)sender).StyleId.EndsWith("A"))
+        {
+            Go(imgTeamA, imgTeamB, imgBall, 
+                (deltaX) => imgTeamA.TranslationX -= deltaX, 
+                (deltaX) => imgBall.TranslationX -= deltaX,
+                (player, item) => player.X > item.X);
+        }
+        else
+        {
+            Go(imgTeamB, imgTeamA, imgBall, 
+                (deltaX) => imgTeamB.TranslationX -= deltaX, 
+                (deltaX) => imgBall.TranslationX -= deltaX,
+                (player, ball) => player.X > ball.X);
+        }
+    }
 
-    private void OnUpClickedTeamB(object sender, EventArgs e) => imgTeamB.TranslationY -= 10;
+    private void OnRightClickedTeam(object sender, EventArgs e)
+    {
+        if (((ImageButton)sender).StyleId.EndsWith("A"))
+        {
+            Go(imgTeamA, imgTeamB, imgBall, 
+                (deltaX) => imgTeamA.TranslationX += deltaX, 
+                (deltaX) => imgBall.TranslationX += deltaX,
+                (player, item) => player.X < item.X);
+        }
+        else
+        {
+            Go(imgTeamB, imgTeamA, imgBall, 
+                (deltaX) => imgTeamB.TranslationX += deltaX, 
+                (deltaX) => imgBall.TranslationX += deltaX,
+                (player, item) => player.X < item.X);
+        }
+    }
 
-    private void OnDownClickedTeamB(object sender, EventArgs e) => imgTeamB.TranslationY += 10;
+    private void Go(
+        Image player, Image opponent, Image ball, 
+        Action<int> playerTranslator, Action<int> ballTranslator, Func<Point, Point, bool> isItemTranslating)
+    {
+        var ballCenter = GetItemCenter(ball);
+        var playerCenter = GetItemCenter(player);
+        var opponentCenter = GetItemCenter(opponent);
 
-    private void OnLeftClickedTeamB(object sender, EventArgs e) => imgTeamB.TranslationX -= 10;
+        // игрок касается мяча
+        //if (Math.Abs((player.WidthRequest + ball.WidthRequest) / 2 - playerCenter.Distance(ballCenter)) <= _collisionEps)
+        if (playerCenter.Distance(ballCenter) <= (player.WidthRequest + ball.WidthRequest) / 2)
+        {
+            // игрок касается соперника
+            //if (Math.Abs(player.WidthRequest - playerCenter.Distance(opponentCenter)) <= _collisionEps)
+            if (playerCenter.Distance(opponentCenter) <= player.WidthRequest)
+            {
+                // соперник НЕ касается мяча
+                //if (Math.Abs((ball.WidthRequest + opponent.WidthRequest) / 2 - opponentCenter.Distance(ballCenter)) > _collisionEps)
+                if (opponentCenter.Distance(ballCenter) > (opponent.WidthRequest + ball.WidthRequest) / 2)
+                {
+                    // игрок двигается в сторону мяча
+                    if (isItemTranslating(playerCenter, ballCenter))
+                    {
+                        // игрок двигается НЕ в сторону соперника
+                        if (!isItemTranslating(playerCenter, opponentCenter))
+                        {
+                            playerTranslator(_translationWithBall);
+                            ballTranslator(_translationWithBall);
+                        }
+                    }
+                    // игрок двигается НЕ в сторону мяча и НЕ в сторону соперника
+                    else if (!isItemTranslating(playerCenter, opponentCenter))
+                    {
+                        playerTranslator(_translationWithOutBall);
+                    }
+                }
+                // соперник касается мяча
+                else
+                {
+                    // игрок двигается в сторону мяча
+                    if (isItemTranslating(playerCenter, ballCenter))
+                    {
+                        // игрок двигается НЕ в сторону соперника
+                        if (!isItemTranslating(playerCenter, opponentCenter))
+                        {
+                            playerTranslator(_translationWithBall);
+                            ballTranslator(_translationWithBall);
+                        }
+                    }
+                    // игрок двигается НЕ в сторону мяча и НЕ в сторону соперника
+                    else if (!isItemTranslating(playerCenter, opponentCenter))
+                    {
+                        playerTranslator(_translationWithOutBall);
+                    }
+                }
+            }
+            // игрок НЕ касается соперника и соперник НЕ касается мяча
+            //else if (Math.Abs((ball.WidthRequest + opponent.WidthRequest) / 2 - opponentCenter.Distance(ballCenter)) > _collisionEps)
+            else if (opponentCenter.Distance(ballCenter) > (opponent.WidthRequest + ball.WidthRequest) / 2)
+            {
+                // игрок двигается в сторону мяча
+                if (isItemTranslating(playerCenter, ballCenter))
+                {
+                    playerTranslator(_translationWithBall);
+                    ballTranslator(_translationWithBall);
+                }
+                else
+                {
+                    playerTranslator(_translationWithOutBall);
+                }
+            }
+            // игрок НЕ касается соперника и соперник касается мяча
+            else
+            {
+                // игрок двигается в сторону мяча
+                if (isItemTranslating(playerCenter, ballCenter))
+                {
+                    // игрок двигается НЕ в сторону соперника
+                    if (!isItemTranslating(playerCenter, opponentCenter))
+                    {
+                        playerTranslator(_translationWithBall);
+                        ballTranslator(_translationWithBall);
+                    }
+                }
+                // игрок двигается НЕ в сторону мяча и НЕ в сторону соперника
+                else if (!isItemTranslating(playerCenter, opponentCenter))
+                {
+                    playerTranslator(_translationWithOutBall);
+                }
+            }
+        }
+        // игрок НЕ касается мяча и касается соперника
+        //else if (Math.Abs(player.WidthRequest - playerCenter.Distance(opponentCenter)) <= _collisionEps)
+        else if (playerCenter.Distance(opponentCenter) <= player.WidthRequest)
+        {
+            // игрок НЕ двигается в сторону соперника
+            if (!isItemTranslating(playerCenter, opponentCenter))
+            {
+                playerTranslator(_translationWithOutBall);
+            }
+        }
+        // просто ходим
+        else
+        {
+            playerTranslator(_translationWithOutBall);
+        }
+    }
 
-    private void OnRightClickedTeamB(object sender, EventArgs e) => imgTeamB.TranslationX += 10;
+    private void OnRightRotate(object sender, EventArgs e)
+    {
+        if (((ImageButton)sender).StyleId.EndsWith("A"))
+            HandleRotateCondition(_angle, imgTeamA, imgTeamB, imgBall);
+        else
+            HandleRotateCondition(_angle, imgTeamB, imgTeamA, imgBall);
+    }
+
+    private void OnLeftRotate(object sender, EventArgs e)
+    {
+        if (((ImageButton)sender).StyleId.EndsWith("A"))
+            HandleRotateCondition(-_angle, imgTeamA, imgTeamB, imgBall);
+        else
+            HandleRotateCondition(-_angle, imgTeamB, imgTeamA, imgBall);
+    }
+
+    private void HandleRotateCondition(double angle, Image player, Image opponent, Image ball)
+    {
+        var ballCenter = GetItemCenter(ball);
+        var playerCenter = GetItemCenter(player);
+        var opponentCenter = GetItemCenter(opponent);
+
+        // игрок касается мяча
+        if (playerCenter.Distance(ballCenter) <= (player.WidthRequest + ball.WidthRequest) / 2)
+        {
+            // соперник НЕ касается мяча
+            if (opponentCenter.Distance(ballCenter) > (opponent.WidthRequest + ball.WidthRequest) / 2)
+            {
+                Rotate(angle, ballCenter, playerCenter);
+            }
+            // соперник касается мяча
+            else
+            {
+                var dx = (ballCenter.X - playerCenter.X) * Math.Cos(angle) -
+                         (ballCenter.Y - playerCenter.Y) * Math.Sin(angle) + playerCenter.X;
+                var dy = (ballCenter.X - playerCenter.X) * Math.Sin(angle) +
+                         (ballCenter.Y - playerCenter.Y) * Math.Cos(angle) + playerCenter.Y;
+
+                var newBallCenter = ballCenter with { X = dx, Y = dy };
+
+                // мяч двигается НЕ в сторону соперника
+                if (newBallCenter.Distance(opponentCenter) > ballCenter.Distance(opponentCenter))
+                {
+                    imgBall.TranslationX = dx - imgBall.X - imgBall.WidthRequest / 2;
+                    imgBall.TranslationY = dy - imgBall.Y - imgBall.HeightRequest / 2;
+                }
+            }
+        }
+    }
+
+    private void Rotate(double angle, Point ballCenter, Point playerCenter)
+    {
+        var dx = (ballCenter.X - playerCenter.X) * Math.Cos(angle) -
+                 (ballCenter.Y - playerCenter.Y) * Math.Sin(angle) + playerCenter.X;
+        var dy = (ballCenter.X - playerCenter.X) * Math.Sin(angle) +
+                 (ballCenter.Y - playerCenter.Y) * Math.Cos(angle) + playerCenter.Y;
+
+        imgBall.TranslationX = dx - imgBall.X - imgBall.WidthRequest / 2;
+        imgBall.TranslationY = dy - imgBall.Y - imgBall.HeightRequest / 2;
+    }
+
+    private Point GetItemCenter(Image item)
+    {
+        return new Point(
+            item.X + item.TranslationX + item.WidthRequest / 2,
+            item.Y + item.TranslationY + item.WidthRequest / 2);
+    }
 }
